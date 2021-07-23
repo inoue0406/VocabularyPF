077300f82ef358ceb77e80f79ecb66f8124efbf6
Tsuyoshi Inoue <inoue0406@gmail.com>
2021-07-24 00:38:05 +0900

    prep for wiktionary

--- prep/prep_wiktionary.py
+++ prep/prep_wiktionary.py
@@ -1,15 +1,33 @@
 # prep for wiktionary data
 
 import json
+import pandas as pd
 #import wiktextract
+
+def extract_pronunciation(sounds):
+    # Extract American Pronunciation
+    #list_country = ["General American","Received Pronunciation","US","UK"]
+    ipa = ""
+    for i in range(len(sounds)):
+        if "ipa" in sounds[i].keys():
+        #if "tags" in sounds[i].keys() and "ipa" in sounds[i].keys():
+            #if sounds[i]['tags'][0] in list_country:
+            ipa = ipa + sounds[i]['ipa']
+    return ipa
 
 if __name__ == '__main__':
 
-    # Wiktionary dump file
+    # Inputs
     
+    # 1: Wiktionary dump file
     fjson = "../data/Wiktionary/kaikki.org-dictionary-English.json"
+    
+    # 2: word list (NGSL)
+    df_words = pd.read_csv('../data/NGSL_Word_Freq_list.csv')
 
-    fcsv = open('../data/out_wordlist_Wiktionary.csv', 'w', encoding='utf-8')
+    # Outputs
+    fcsv = open('../data/out_wordlist_NGSL_Wiktionary.csv', 'w', encoding='utf-8')
+
     print("word,sound,sense",file=fcsv)
     
     with open(fjson, "r") as f:
@@ -19,14 +37,21 @@ if __name__ == '__main__':
             # extract data
             if "word" in data.keys():
                 word = data['word']
+                
+                if word == "pyre":
+                    #import pdb;pdb.set_trace()
+                    pass
+                
+                # if contained in NGSL list
+                if sum(df_words["Lemma"] == word)==0:
+                    print(word,"not contained in NGSL. Skip.")
+                    continue
+                
                 print("processing word:",word)
                 if "sounds" in data.keys():
-                    if "ipa" in data['sounds'][0]:
-                        sound = data['sounds'][0]["ipa"]
-                    else:
-                        sound = "NA"
+                    sound =extract_pronunciation(data["sounds"])
                 else:
-                    sound = "NA"
+                    sound = ""
                 if "senses" in data.keys():
                     if "glosses" in data['senses'][0]:
                         sense = data['senses'][0]['glosses'][0]
@@ -34,10 +59,11 @@ if __name__ == '__main__':
                         sense = "NA"
                 else:
                     sense = "NA"
-                print("%s,%s,%s" % (word,sound,sense),file=fcsv)
+                #import pdb;pdb.set_trace()
+                print("%s,%s,'%s'" % (word,sound,sense),file=fcsv)
                 #lst.append(data)
                 count = count + 1
                 #if count > 100:
-                #    import pdb;pdb.set_trace()
+
 
     
